c5e8ecc0c91cd8deafa40388168fba08dafdbd73
Chris Wewerka <breadfan@gmx.de>
2011-02-10 22:01:57 +0100

    added first steps

--- src/de/threedimensions/blog/client/Blog.java
+++ src/de/threedimensions/blog/client/Blog.java
@@ -1,28 +1,92 @@
 package de.threedimensions.blog.client;
 
+import java.util.Date;
+
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.event.dom.client.BlurEvent;
+import com.google.gwt.event.dom.client.BlurHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SplitLayoutPanel;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import de.threedimensions.blog.shared.BlogEntry;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Blog implements EntryPoint {
 
-	private SplitLayoutPanel mainPanel = new SplitLayoutPanel();
-	private FlexTable flexTable = new FlexTable();
+    /**
+     * Entry point method.
+     */
+    public void onModuleLoad() {
+	HorizontalPanel mainPanel = new HorizontalPanel();
 
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		flexTable.add(new HTML("blog1"));
-		flexTable.add(new HTML("blog2"));
-		mainPanel.add(flexTable);
+	VerticalPanel contentMiddlePanel = new VerticalPanel();
+	contentMiddlePanel.add(getCurrentBlogEntry());
+	mainPanel.add(contentMiddlePanel);
 
-		// Associate the Main panel with the HTML host page.
-		RootPanel.get("blog").add(mainPanel);
-	}
+	HorizontalPanel commentPanel = createCommentPanel();
+	contentMiddlePanel.add(commentPanel);
+
+	VerticalPanel navPanel = createNavPanel();
+	mainPanel.add(navPanel);
+
+	RootPanel.get("blogPanel").add(mainPanel);
+    }
+
+    /**
+     * @return
+     */
+    private Widget getCurrentBlogEntry() {
+	BlogEntry blogEntry = new BlogEntry(
+		"Heading 1",
+		"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+		new Date());
+	return new HTML("<h2>" + blogEntry.getHeading() + "</h2>" + blogEntry.getContent());
+    }
+
+    private VerticalPanel createNavPanel() {
+	VerticalPanel navPanel = new VerticalPanel();
+	navPanel.add(new Hyperlink("Post 1", "post1"));
+	navPanel.add(new Hyperlink("Post 2", "post2"));
+	return navPanel;
+    }
+
+    private HorizontalPanel createCommentPanel() {
+	HorizontalPanel commentPanel = new HorizontalPanel();
+	final TextArea commentInputTextArea = new TextArea();
+	commentInputTextArea.setVisibleLines(1);
+	commentInputTextArea.addFocusHandler(new FocusHandler() {
+	    @Override
+	    public void onFocus(FocusEvent event) {
+		commentInputTextArea.setVisibleLines(5);
+	    }
+	});
+	commentInputTextArea.addBlurHandler(new BlurHandler() {
+
+	    @Override
+	    public void onBlur(BlurEvent event) {
+		commentInputTextArea.setVisibleLines(1);
+	    }
+	});
+	commentPanel.add(commentInputTextArea);
+
+	Button commentButton = new Button("Post a comment", new ClickHandler() {
+	    public void onClick(ClickEvent event) {
+		commentInputTextArea.setVisible(true);
+	    }
+	});
+	commentPanel.add(commentButton);
+	return commentPanel;
+    }
 }
---
+++ src/de/threedimensions/blog/shared/BlogEntry.java
@@ -0,0 +1,71 @@
+package de.threedimensions.blog.shared;
+
+import java.util.Date;
+
+/**
+ * @author Chris Wewerka
+ * 
+ */
+public class BlogEntry {
+    private String heading;
+    private String content;
+    private Date creationTime;
+
+    /**
+     * @param heading
+     * @param content
+     * @param creationTime
+     */
+    public BlogEntry(String heading, String content, Date creationTime) {
+	super();
+	this.heading = heading;
+	this.content = content;
+	this.creationTime = creationTime;
+    }
+
+    /**
+     * @return the heading
+     */
+    public String getHeading() {
+	return heading;
+    }
+
+    /**
+     * @param heading
+     *            the heading to set
+     */
+    public void setHeading(String heading) {
+	this.heading = heading;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+	return content;
+    }
+
+    /**
+     * @param content
+     *            the content to set
+     */
+    public void setContent(String content) {
+	this.content = content;
+    }
+
+    /**
+     * @return the creationTime
+     */
+    public Date getCreationTime() {
+	return creationTime;
+    }
+
+    /**
+     * @param creationTime
+     *            the creationTime to set
+     */
+    public void setCreationTime(Date creationTime) {
+	this.creationTime = creationTime;
+    }
+
+}
