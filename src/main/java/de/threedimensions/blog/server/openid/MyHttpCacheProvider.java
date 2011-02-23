ede477d3a9485214cc7e57ce6b830138e690762e
beadfan@gmx.de <beadfan@gmx.de>
2011-02-23 18:02:28 +0100

    draft of integration of openid-springsec-gwt blog article

---
+++ src/main/java/de/threedimensions/blog/server/openid/MyHttpCacheProvider.java
@@ -0,0 +1,16 @@
+package de.threedimensions.blog.server.openid;
+
+import org.openid4java.util.HttpFetcher;
+
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
+import com.google.inject.Provider;
+
+public class MyHttpCacheProvider implements Provider<HttpFetcher>
+{
+
+	@Override
+	public HttpFetcher get()
+	{
+		return new Openid4javaFetcher(URLFetchServiceFactory.getURLFetchService());
+	}
+}
