fc6249e40472b655becffae393f3bde111c1e674
Chris Wewerka <breadfan@gmx.de>
2011-02-25 09:06:01 +0100

    added openid first own steps to app

--- src/main/java/de/threedimensions/blog/server/BlogController.java
+++ src/main/java/de/threedimensions/blog/server/BlogController.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.openid4java.OpenIDException;
+import org.openid4java.consumer.ConsumerManager;
+import org.openid4java.discovery.DiscoveryInformation;
+import org.openid4java.message.AuthRequest;
+import org.openid4java.message.ax.FetchRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +21,9 @@ import de.threedimensions.blog.server.model.BlogEntryRef;
 @Controller
 @RequestMapping("/*")
 public class BlogController {
+
+    // instantiate a ConsumerManager object
+    ConsumerManager openIdConsumerManager = new ConsumerManager();
 
     @RequestMapping(value = "/posts", method = RequestMethod.GET)
     public @ResponseBody
@@ -33,5 +41,46 @@ public class BlogController {
 		new Date());
 	blogEntry.setId(postId);
 	return blogEntry;
+    }
+
+    @RequestMapping(value = "/auth/{userSuppliedString}", method = RequestMethod.POST)
+    public String doOpenIdLogin(@PathVariable String userSuppliedString) {
+	try {
+	    // configure the return_to URL where your application will receive
+	    // the authentication responses from the OpenID provider
+	    String returnToUrl = "http://threedimensionsblog.appspot.com/app/auth";
+
+	    // perform discovery on the user-supplied identifier
+	    List discoveries = openIdConsumerManager.discover(userSuppliedString);
+
+	    // attempt to associate with the OpenID provider
+	    // and retrieve one service endpoint for authentication
+	    DiscoveryInformation discovered = openIdConsumerManager.associate(discoveries);
+
+	    // store the discovery information in the user's session
+	    // httpReq.getSession().setAttribute("openid-disc", discovered);
+
+	    // obtain a AuthRequest message to be sent to the OpenID provider
+	    AuthRequest authReq = openIdConsumerManager.authenticate(discovered, returnToUrl);
+
+	    // Attribute Exchange example: fetching the 'email' attribute
+	    FetchRequest fetch = FetchRequest.createFetchRequest();
+	    fetch.addAttribute("email",
+	    // attribute alias
+		    "http://schema.openid.net/contact/email", // type URI
+		    true); // required
+
+	    // attach the extension to the authentication request
+	    authReq.addExtension(fetch);
+
+	    // Option 1: GET HTTP-redirect to the OpenID Provider endpoint
+	    // The only method supported in OpenID 1.x
+	    // redirect-URL usually limited ~2048 bytes
+	    return (authReq.getDestinationUrl(true));
+
+	} catch (OpenIDException e) {
+	    // present error to the user
+	    throw new RuntimeException(e);
+	}
     }
 }
