2ba55be0b427f97c74e6ab2ff77ace9367a815b4
Chris Wewerka <breadfan@gmx.de>
2011-03-24 01:10:15 +0100

    added Repo annot to DAO

--- src/main/java/de/threedimensions/blog/server/dao/UserDao.java
+++ src/main/java/de/threedimensions/blog/server/dao/UserDao.java
@@ -3,12 +3,15 @@ package de.threedimensions.blog.server.dao;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+
 import de.threedimensions.blog.server.model.User;
 
 /**
  * @author chris
  * 
  */
+@Repository
 public class UserDao {
 
     @PersistenceContext
